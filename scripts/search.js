b646760204a38cf14fe5f0f133b7bbe344695881
Wronqa <kuba.wrona@onet.pl>
2023-07-07 22:46:36 +0200

    Created main function to handle search

--- scripts/search.js
+++ scripts/search.js
@@ -1,2 +1,22 @@
 const cityInput = document.querySelector('select');
 const searchBtn = document.querySelector('button');
+
+console.log('test');
+
+if (searchBtn) {
+	searchBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		const city = cityInput.value;
+
+		getForecast(city).then((data) => {
+			console.log(data);
+			document.querySelector('.result').innerHTML = `
+        Data pomiaru: ${data.godzina_pomiaru} </br>
+        Temperatura: ${data.temperatura}  </br>
+        Suma opadu: ${data.suma_opadu}  </br>
+        Ciśnienie: ${data.cisnienie}  </br>
+
+      `;
+		});
+	});
+}

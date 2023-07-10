49c98ac7b652d659a4e0923d92e5ad455ac2d36d
Wronqa <kuba.wrona@onet.pl>
2023-07-10 14:36:47 +0200

    Added get dat from local storage

--- scripts/search.js
+++ scripts/search.js
@@ -1,13 +1,32 @@
+//Load all nessesary elements
+
 const cityInput = document.querySelector('select');
 const searchBtn = document.querySelector('button');
+
+//Check if exist recent searching data
+
+window.onload = () => {
+	const recentData = localStorage.getItem('weatherData');
+
+	recentData && showData(JSON.parse(recentData));
+};
+
+//Detect click on button
 
 if (searchBtn) {
 	searchBtn.addEventListener('click', async (e) => {
 		e.preventDefault();
 		const city = cityInput.value;
 
+		if (!city.trim().length) {
+			return showError('Wybierz miasto');
+		}
+
 		const data = await getForecast(city);
 
-		console.log(data);
+		if (data) {
+			localStorage.setItem('weatherData', JSON.stringify(data));
+			showData(data);
+		}
 	});
 }

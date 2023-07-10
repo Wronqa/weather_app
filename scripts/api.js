b701442f350db49a63f08fbefab4dd041c2b4104
Wronqa <kuba.wrona@onet.pl>
2023-07-10 13:52:22 +0200

    Add error handling

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,14 @@
 const apiUrl = 'https://danepubliczne.imgw.pl/api/data/synop/station/';
 
 const getForecast = async (city) => {
-	const response = await fetch(apiUrl.concat(city));
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(apiUrl.concat(city));
+		const data = await response.json();
+
+		if (data.status === false) throw new Error(data.message);
+
+		return data;
+	} catch (err) {
+		console.log(err.message);
+	}
 };

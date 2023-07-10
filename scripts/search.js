68f443e5f321a8956ed4234fa6ee260be3d6b685
Wronqa <kuba.wrona@onet.pl>
2023-07-10 13:52:37 +0200

    Modified api call structure

--- scripts/search.js
+++ scripts/search.js
@@ -1,22 +1,13 @@
 const cityInput = document.querySelector('select');
 const searchBtn = document.querySelector('button');
 
-console.log('test');
-
 if (searchBtn) {
-	searchBtn.addEventListener('click', (e) => {
+	searchBtn.addEventListener('click', async (e) => {
 		e.preventDefault();
 		const city = cityInput.value;
 
-		getForecast(city).then((data) => {
-			console.log(data);
-			document.querySelector('.result').innerHTML = `
-        Data pomiaru: ${data.godzina_pomiaru} </br>
-        Temperatura: ${data.temperatura}  </br>
-        Suma opadu: ${data.suma_opadu}  </br>
-        Ciśnienie: ${data.cisnienie}  </br>
+		const data = await getForecast(city);
 
-      `;
-		});
+		console.log(data);
 	});
 }
